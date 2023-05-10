450cf04ec2b12374c4a0044efea77fe82c60ab14
Yohei Yasukawa <yohei@yasslab.jp>
2023-05-10 04:47:20 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,40 @@
 class UsersController < ApplicationController
+  # GET /users/:id
+  def show
+    #@user = User.first
+    @user = User.find(params[:id])
+
+    # => app/views/users/show.html.erb
+  end
+
+  # GET /users/new or /signup
   def new
-    # => GET app/views/users/new.html.erb
+    @user = User.new
+
+    # render 'new'
+    # => app/views/users/new.html.erb
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # => Success / alert-success
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user # => GET /users/:id
+      #redirect_to user_path(@user)    # => GET /users/:id
+      #redirect_to user_path(@user.id) # => GET /users/2
+    else
+      # => Failure
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # Strong Parameters
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,10 @@ Rails.application.routes.draw do
   get  "/about",   to: "static_pages#about"
   get  "/contact", to: "static_pages#contact"
   get  "/signup",  to: "users#new"
+
+  resources :users
+  # => get "/users",     to: "users#index"
+  # => get "/users/:id", to: "users#show"
+  # => get "/users/new", to: "users#new"
+  # ...
 end
